86d088278d88fc8bc6205141ee519dd9953d84e4
Gustavo Gabriel De Luca <gustavodeluca96@hotmail.com>
2019-10-23 20:04:20 -0300

    Fix eslint

--- src/services/coreAPI.js
+++ src/services/coreAPI.js
@@ -1,15 +1,11 @@
 const { UserNotFound } = require('../models/Error/Core');
 
 async function getUser(email, password) {
-    try {
-        if (email === 'pepe@gmail.com' && password === '12345678') {
-            return await Promise.resolve({ name: 'pepe' });
-        }
-
-        throw new UserNotFound('User not Found');
-    } catch(err) {
-        throw err;
+    if (email === 'pepe@gmail.com' && password === '12345678') {
+        return await Promise.resolve({ name: 'pepe' });
     }
+
+    throw new UserNotFound('User not Found');
 }
 
 async function userExists(email, password) {
